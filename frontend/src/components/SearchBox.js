4032c48b1c91814ead88027f1cf82d21ea045e0d
Jennylyn Gobaton <jennylyn.gobaton@outlook.com>
2021-10-01 00:10:38 +0800

    Product pagination, searchbox style edit

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState('')
@@ -15,16 +15,23 @@ const SearchBox = ({ history }) => {
 
   return (
     <Form onSubmit={submitHandler} inline>
-      <Form.Control
-        type='text'
-        name='q'
-        onChange={(e) => setKeyword(e.target.value)}
-        placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'
-      ></Form.Control>
-      <Button type='submit' variant='outline-sucsess' className='p-2'>
-        Search
-      </Button>
+      <Row>
+        <Col>
+          <Form.Control
+            type='text'
+            name='q'
+            onChange={(e) => setKeyword(e.target.value)}
+            placeholder='Search Products...'
+            className='mr-sm-2 ml-sm-5'
+            style={{ width: '300px' }}
+          ></Form.Control>
+        </Col>
+        <Col>
+          <Button type='submit' variant='outline-success' className='p-2'>
+            Search
+          </Button>
+        </Col>
+      </Row>
     </Form>
   )
 }
